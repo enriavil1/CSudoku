0d49b6e492ecd551b4af72f4ed079709458ad121
Enrique Avila Asapche <eavilaasapche@gmail.com>
2022-03-09 18:35:42 +0000

    added single line checking for efficiency

--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -25,22 +25,26 @@ void Sudoku::print_board() {
         std::cout << "|\n";
         // every three rows it will add a line beetwen the rows
         if((i+1)%3==0){
-            for(int k = 0; k < Sudoku::board_[i].size()*3; ++k){
-                std::cout << "-";
+            for(int k = 0; k < Sudoku::board_[i].size(); ++k){
+                std::cout << "---";
             }
             std::cout << "\n";
         }
+    }
+}
 
-        
+bool Sudoku::is_board_valid() {
+    bool is_valid = Sudoku::are_rows_valid() && Sudoku::are_columns_valid() && Sudoku::are_blocks_valid();
+
+    if(!is_valid) {
+        std::cout << "BOARD IS NOT VALID\n";
+        Sudoku::~Sudoku();
     }
 
+    return Sudoku::are_rows_valid() && Sudoku::are_columns_valid() && Sudoku::are_blocks_valid();
 }
 
-bool Sudoku::is_valid() {
-    return Sudoku::is_row_valid() && Sudoku::is_column_valid() && Sudoku::is_block_valid();
-}
-
-bool Sudoku::is_row_valid() {
+bool Sudoku::are_rows_valid() {
     for(int i = 0; i < Sudoku::board_.size(); ++i){
         std::map<int, bool> row;
         for(int j = 0; j < Sudoku::board_[i].size(); ++j){
@@ -53,7 +57,7 @@ bool Sudoku::is_row_valid() {
     return true;
 }
 
-bool Sudoku::is_column_valid() {
+bool Sudoku::are_columns_valid() {
     for(int i = 0; i < Sudoku::board_.size(); ++i) {
         std::map<int, bool> column;
         for(int j = 0; j < Sudoku::board_.size(); ++j) {
@@ -67,11 +71,51 @@ bool Sudoku::is_column_valid() {
     return true;
 }
 
-bool Sudoku::is_block_valid() {
+bool Sudoku::are_blocks_valid() {
     return true;
 }
 
-void Sudoku::solve() {
-    return ;
+bool Sudoku::find_empty_spot() {
+    for(int i = 0; i < Sudoku::board_.size(); ++i) {
+        for(int j = 0; j < Sudoku::board_.size(); ++j) {
+            if(Sudoku::board_[i][j] == 0 ){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Sudoku::is_row_valid(int& num) {
+    for(Sudoku::col = 0; Sudoku::col < 9; ++col){
+        if(Sudoku::board_[Sudoku::row][Sudoku::col] == num){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+bool Sudoku::solve() {
+    Sudoku::print_board();
+
+    if(!Sudoku::find_empty_spot()){
+        Sudoku::print_board();
+        std::cout << "\nBOARD HAS BEEN COMPLETES\n";
+        return true;
+    }
+
+    for(int num = 1; num < 9; ++num) {
+        if(Sudoku::is_valid_place(num)){
+            Sudoku::board_[Sudoku::row][Sudoku::col] = num;
+
+            if(Sudoku::solve()){
+                return true;
+            }
+
+            Sudoku::board_[Sudoku::row][Sudoku::col] = 0;
+        }
+    }
+    return false;
 }
 
--- sudoku/sudoku.hpp
+++ sudoku/sudoku.hpp
@@ -3,20 +3,27 @@
 class Sudoku
 {
 public:
-    Sudoku(std::vector<std::vector<int> >& board): board_(board) {}
+    Sudoku(std::vector< std::vector<int> >& board): board_(board), row(0), col(0) {}
     ~Sudoku() {};
     void print_board();
-    void solve();
+    bool solve();
 
+    bool is_board_valid();
+    bool are_rows_valid();
+    bool are_columns_valid();
+    bool are_blocks_valid();
+
+    bool find_empty_spot();
     bool is_valid();
-    bool is_row_valid();
-    bool is_column_valid();
-    bool is_block_valid();
+    bool is_row_valid(int& num);
+    bool is_column_valid(int& num);
+    bool is_block_valid(int& num);
 
     
 
 private:
-    std::vector<std::vector<int> >& board_;
-
+    std::vector< std::vector<int> >& board_;
+    int row;
+    int col;
 };
 
