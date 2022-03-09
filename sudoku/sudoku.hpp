8ae7c7343ccaaa5d81829f87784ceb58b6424c98
Enrique Avila Asapche <eavilaasapche@gmail.com>
2022-03-09 20:19:41 +0000

    deleted row and col attributes

--- sudoku/sudoku.hpp
+++ sudoku/sudoku.hpp
@@ -14,17 +14,15 @@ public:
     bool are_columns_valid();
     bool are_blocks_valid();
 
-    bool find_empty_spot();
-    bool is_valid_place(int row, int col, const int& num);
-    bool is_row_valid(const int& num);
-    bool is_column_valid(const int& num);
-    bool is_block_valid(int row, int col, const int& num);
+    bool find_empty_spot(int& row, int& col);
+    bool is_valid_place(const int& row, const int& col, const int& num);
+    bool is_row_valid(const int&row, const int& num);
+    bool is_column_valid(const int& col, const int& num);
+    bool is_block_valid(const int& row, const int& col, const int& num);
 
     
 
 private:
     std::vector< std::vector<int> > board_;
-    int row;
-    int col;
 };
 
